ac427766250f7708ad8e96394c54069129d87305
Eli <117216467+readytotest@users.noreply.github.com>
2023-10-27 07:50:49 -0700

    Update orangeDemoFakeDataGenerator.js

--- cypress/fixtures/orange-demo/orangeDemoFakeDataGenerator.js
+++ cypress/fixtures/orange-demo/orangeDemoFakeDataGenerator.js
@@ -1,5 +1,3 @@
-/* Need to look into another alternative for random data
-like https://fakerjs.dev/ when I get time */
 const numRandom = () => Math.floor((Math.random() + 1) * 99);
 const insertNum = numRandom();
 const randomEmailGenerator = `john.smith999${insertNum}@fake-email-999${insertNum}.org`;
