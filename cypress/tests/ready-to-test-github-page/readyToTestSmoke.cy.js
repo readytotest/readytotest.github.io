a9343c03db17fac9d8165d9518b97776f9a5f2f3
readytotest <117216467+readytotest@users.noreply.github.com>
2023-10-26 20:08:42 -0700

    fix gh page smoke test

--- cypress/tests/ready-to-test-github-page/readyToTestSmoke.cy.js
+++ cypress/tests/ready-to-test-github-page/readyToTestSmoke.cy.js
@@ -11,7 +11,7 @@ describe('Smoke Test GitHub Page', () => {
         cy.get('[data-cy="cypress-report"]').click() 
         cy.get('center').should('include.text', 'Click on the test results above ^^ to expand it and view the video')
         cy.get('[href="/index.htm"]').click() 
-        cy.get('body').should('include.text', 'Cypress Test Automation Experimental Site')
+        cy.get('body').should('include.text', 'Cypress Test Automation')
     })
 
   })
