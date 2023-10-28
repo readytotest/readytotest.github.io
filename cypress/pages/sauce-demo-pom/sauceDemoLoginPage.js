478b26900ffface8d9f232933699d21d3718c22f
readytotest <117216467+readytotest@users.noreply.github.com>
2023-10-28 07:56:00 -0700

    Add login only test

--- cypress/pages/sauce-demo-pom/sauceDemoLoginPage.js
+++ cypress/pages/sauce-demo-pom/sauceDemoLoginPage.js
@@ -4,6 +4,7 @@ class sauceDemoLoginPage{
         usernameLogin: () => cy.get('#user-name'),
         passwordLogin: () => cy.get('#password'),
         buttonLogin: () => cy.get('#login-button'),
+        loginErrorMessage: () => cy.get('h3[data-test="error"]')
     }
 
     enterUsername(username){
@@ -20,6 +21,22 @@ class sauceDemoLoginPage{
 
     confirmLoginButtonExists(){
         this.elements.buttonLogin().should('exist');
+    }
+
+    confirmLockedOutMessage(){
+        this.elements.loginErrorMessage().should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
+    }
+
+    confirmEmptyUsernameMessage(){
+        this.elements.loginErrorMessage().should('have.text', 'Epic sadface: Username is required');
+    }
+
+    confirmEmptyPasswordMessage(){
+        this.elements.loginErrorMessage().should('have.text', 'Epic sadface: Password is required');
+    }
+
+    confirmInvalidUsernameOrPasswordMessage(){
+        this.elements.loginErrorMessage().should('have.text', 'Epic sadface: Username and password do not match any user in this service');
     }
 
 }
---
+++ cypress/tests/sauce-demo-site/sauceDemoLoginOnlyTest.cy.js
@@ -0,0 +1,61 @@
+import sauceDemoLoginPage from '../../pages/sauce-demo-pom/sauceDemoLoginPage';
+import sauceDemoTestData from '../../fixtures/sauce-demo/sauceDemoTestData.json';
+import sauceDemoProductsPage from '../../pages/sauce-demo-pom/sauceDemoProductsPage';
+
+describe('Login Tests', () => {
+
+    beforeEach(() => {
+
+        cy.visit(Cypress.env('sauceDemoUrl'));
+              
+      });
+
+    it('Valid username/password', () => {
+
+        sauceDemoLoginPage.enterUsername(sauceDemoTestData.loginCredentials.usernameValid);
+        sauceDemoLoginPage.enterPassword(sauceDemoTestData.loginCredentials.passwordValid);
+        sauceDemoLoginPage.clickLoginButton();      
+        sauceDemoProductsPage.confirmOnProductPage();
+    })
+
+    it('Locked out user', () => {
+
+        sauceDemoLoginPage.enterUsername(sauceDemoTestData.loginCredentials.usernameLockedOut);
+        sauceDemoLoginPage.enterPassword(sauceDemoTestData.loginCredentials.passwordValid);
+        sauceDemoLoginPage.clickLoginButton();
+        sauceDemoLoginPage.confirmLockedOutMessage();
+    })
+
+    it('Empty username', () => {
+
+        sauceDemoLoginPage.enterPassword(sauceDemoTestData.loginCredentials.passwordValid);
+        sauceDemoLoginPage.clickLoginButton();
+        sauceDemoLoginPage.confirmEmptyUsernameMessage();
+    })
+
+    it('Empty password', () => {
+
+        sauceDemoLoginPage.enterUsername(sauceDemoTestData.loginCredentials.usernameValid);
+        sauceDemoLoginPage.clickLoginButton();
+        sauceDemoLoginPage.confirmEmptyPasswordMessage();
+    })
+
+    it('Invalid username', () => {
+
+        sauceDemoLoginPage.enterUsername(sauceDemoTestData.loginCredentials.usernameInvalid);
+        sauceDemoLoginPage.enterPassword(sauceDemoTestData.loginCredentials.passwordValid);
+        sauceDemoLoginPage.clickLoginButton();
+        sauceDemoLoginPage.confirmInvalidUsernameOrPasswordMessage();
+    })
+
+    it('Invalid password', () => {
+
+        sauceDemoLoginPage.enterUsername(sauceDemoTestData.loginCredentials.usernameValid);
+        sauceDemoLoginPage.enterPassword(sauceDemoTestData.loginCredentials.passwordInvalid);
+        sauceDemoLoginPage.clickLoginButton();
+        sauceDemoLoginPage.confirmInvalidUsernameOrPasswordMessage();
+    })
+
+    
+});
+
