5b12b6ca6e922c049528b18ade40f95a8396966e
readytotest <117216467+readytotest@users.noreply.github.com>
2023-11-16 10:14:05 -0800

    update html

--- js/repoLastCommitInfo.js
+++ js/repoLastCommitInfo.js
@@ -4,7 +4,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
  if (this.readyState == 4 && this.status == 200) {
     let repos = JSON.parse(this.responseText);
-    writeMain.innerHTML = ("Repository Last Commit<br>");
+    writeMain.innerHTML = ("Repositories Most Recent Commit Timestamp<br>");
     repos.forEach((repo)=>{
     writeMain.insertAdjacentHTML('beforeend', `<a href="${repo.html_url}">${repo.name}: ${new Date(repo.pushed_at)}</a><br>`);
     });
