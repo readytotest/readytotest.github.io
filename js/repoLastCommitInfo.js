c6abbd6dcdc393376b3b358e21f1dd6723262517
readytotest <117216467+readytotest@users.noreply.github.com>
2023-11-16 08:05:09 -0800

    add repo last commit timestamp

---
+++ js/repoLastCommitInfo.js
@@ -0,0 +1,14 @@
+let writeMain = document.querySelector('main');
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+ if (this.readyState == 4 && this.status == 200) {
+    let repos = JSON.parse(this.responseText);
+    writeMain.innerHTML = ("Repository Last Commit<br>");
+    repos.forEach((repo)=>{
+    writeMain.insertAdjacentHTML('beforeend', `${repo.name}: ${new Date(repo.pushed_at)}<br>`);
+    });
+ }
+};
+xhttp.open("GET", "https://api.github.com/users/readytotest/repos", true);
+xhttp.send();
