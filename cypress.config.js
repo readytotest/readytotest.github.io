9d98cb9d1c8c5d28ebd15cd9a53a063fef477bc4
readytotest <117216467+readytotest@users.noreply.github.com>
2023-11-20 08:25:05 -0800

    Add Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "dm22wp", //for Cypress Cloud
   reporter: 'cypress-mochawesome-reporter',
   video: true,
   numTestsKeptInMemory: 5,
