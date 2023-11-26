664faa890c12917add32f40a5a24b4950b2df325
readytotest <117216467+readytotest@users.noreply.github.com>
2023-11-26 09:20:57 -0800

    comment out loop and update html text

--- cypress/tests/ready-to-test-github-page/readyToTestSmoke.cy.js
+++ cypress/tests/ready-to-test-github-page/readyToTestSmoke.cy.js
@@ -9,13 +9,13 @@ describe('Smoke Test GitHub Page', () => {
      
    it('Go to reports page and return to index page', () => {
     //don't need this for loop in here, just wanted to tinker ;)
-    var i = 0;
-    for (i = 0; i < 4 ; i++) { 
+    // var i = 0;
+    // for (i = 0; i < 4 ; i++) { 
         cy.get('[data-cy="cypress-report"]').click() 
         cy.get('center').should('include.text', 'Click on the test results above ^^ to expand it and view the video')
         cy.get('[href="/index.htm"]').click() 
         cy.get('body').should('include.text', 'Cypress Test Automation')
-    }
+    // }
     })
   
 
