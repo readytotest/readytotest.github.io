b6512c351c732e97db784a148b665bfa1663d9cf
readytotest <117216467+readytotest@users.noreply.github.com>
2023-11-28 08:10:16 -0800

    add earthquake info

---
+++ js/earthQuake.js
@@ -0,0 +1,26 @@
+fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson') 
+.then(response => response.json()) 
+
+.then(response => { document.querySelector("earthquake-place").textContent = (response.features[0]?.properties.place)
+return response;
+})
+
+.then(response => { document.querySelector("earthquake-mag").textContent = (response.features[0]?.properties.mag)
+return response;
+})
+
+.then(response => { document.querySelector("earthquake-time").textContent = (response.features[0]?.properties.time)
+return response;
+})
+
+.then(response => { document.querySelector("earthquake-long").textContent = (response.features[0]?.geometry.coordinates[0])
+return response;
+})
+
+.then(response => { document.querySelector("earthquake-lat").textContent = (response.features[0]?.geometry.coordinates[1])
+return response;
+})
+
+.then(response => { document.querySelector("earthquake-depth").textContent = (response.features[0]?.geometry.coordinates[2])
+return response;
+})
