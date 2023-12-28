5cd50f47a4b628ec34dace542f6d07c9ae16400a
readytotest <117216467+readytotest@users.noreply.github.com>
2023-12-28 08:21:44 -0800

    add more to GitHub page test

--- cypress/tests/ready-to-test-github-page/readyToTestSmoke.cy.js
+++ cypress/tests/ready-to-test-github-page/readyToTestSmoke.cy.js
@@ -7,15 +7,22 @@ describe('Smoke Test GitHub Page', () => {
       
       })
      
-   it('Go to reports page and return to index page', () => {
-    //don't need this for loop in here, just wanted to tinker ;)
-    // var i = 0;
-    // for (i = 0; i < 4 ; i++) { 
+   it('Check for text on page, go to reports page, check for HTML elements', () => {
         cy.get('[data-cy="cypress-report"]').click() 
         cy.get('center').should('include.text', 'Click on the test results above ^^ to expand it and view the video')
         cy.get('[href="/index.htm"]').click() 
-        cy.get('body').should('include.text', 'Cypress Test Automation')
-    // }
+        cy.get('body').should('include.text', 'Ready to Test (QA) GitHub Profile')
+        cy.get('body').should('include.text', 'Cypress Test Automation Scripts')
+        cy.get('body').should('include.text', 'Cypress Cloud Report')
+        cy.get('body').should('include.text', 'GitHub Actions in My Repo')
+        cy.get('body').should('include.text', 'National Weather Service API')
+        cy.get('body').should('include.text', 'Postman API Testing')
+        cy.get('body').should('include.text', 'Most Recent Earthquake')
+        cy.get('earthquake-place').should('exist')
+        cy.get('body').should('include.text', 'Repository Last Commit')
+        cy.get('repo-update-timestamp').should('exist')
+
+    
     })
   
 
