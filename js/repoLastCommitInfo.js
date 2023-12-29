be28fdc595e019f21bc8217c3d7b25334e1131b3
readytotest <117216467+readytotest@users.noreply.github.com>
2023-12-28 17:37:19 -0800

    add emojis

--- js/repoLastCommitInfo.js
+++ js/repoLastCommitInfo.js
@@ -4,7 +4,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
  if (this.readyState == 4 && this.status == 200) {
     let repos = JSON.parse(this.responseText);
-    writeMain.innerHTML = ("Repository Last Commit<br>");
+    writeMain.innerHTML = ("Repository Last Commit 💾<br>");
     repos.forEach((repo)=>{
     writeMain.insertAdjacentHTML('beforeend', `<a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">${repo.name}</a>: ${new Date(repo.pushed_at)}<br>`);
     });
