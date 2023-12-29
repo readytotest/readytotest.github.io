b5790076b41ba15e0736262e43a25f468c79c43a
readytotest <117216467+readytotest@users.noreply.github.com>
2023-12-28 17:59:31 -0800

    add link to script

--- js/repoLastCommitInfo.js
+++ js/repoLastCommitInfo.js
@@ -4,7 +4,7 @@ const myRepoInfo = new XMLHttpRequest();
 myRepoInfo.onreadystatechange = function() {
  if (this.readyState == 4 && this.status == 200) {
     let repoData = JSON.parse(this.responseText);
-    writeRepoTimeStamp.innerHTML = ("Repository Last Commit 💾<br>");
+    writeRepoTimeStamp.innerHTML = (`Repository Last Commit 💾<br><a href="https://github.com/readytotest/readytotest.github.io/blob/main/js/repoLastCommitInfo.js" target="_blank" rel="noopener noreferrer">repoLastCommitInfo.js</a><br>`);
     repoData.forEach((repo)=>{
     writeRepoTimeStamp.insertAdjacentHTML('beforeend', `<a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">${repo.name}</a>: ${new Date(repo.pushed_at)}<br>`);
     });
