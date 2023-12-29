b08ea629a43dccc6b2b67ea61ef464defd216824
readytotest <117216467+readytotest@users.noreply.github.com>
2023-12-28 17:51:52 -0800

    update repo last commit

--- js/repoLastCommitInfo.js
+++ js/repoLastCommitInfo.js
@@ -1,14 +1,14 @@
-let writeMain = document.querySelector('repo-update-timestamp');
+const writeRepoTimeStamp = document.querySelector('repo-update-timestamp');
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+const myRepoInfo = new XMLHttpRequest();
+myRepoInfo.onreadystatechange = function() {
  if (this.readyState == 4 && this.status == 200) {
-    let repos = JSON.parse(this.responseText);
-    writeMain.innerHTML = ("Repository Last Commit 💾<br>");
-    repos.forEach((repo)=>{
-    writeMain.insertAdjacentHTML('beforeend', `<a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">${repo.name}</a>: ${new Date(repo.pushed_at)}<br>`);
+    let repoData = JSON.parse(this.responseText);
+    writeRepoTimeStamp.innerHTML = ("Repository Last Commit 💾<br>");
+    repoData.forEach((repo)=>{
+    writeRepoTimeStamp.insertAdjacentHTML('beforeend', `<a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">${repo.name}</a>: ${new Date(repo.pushed_at)}<br>`);
     });
  }
 };
-xhttp.open("GET", "https://api.github.com/users/readytotest/repos", true);
-xhttp.send();
+myRepoInfo.open("GET", "https://api.github.com/users/readytotest/repos", true);
+myRepoInfo.send();
