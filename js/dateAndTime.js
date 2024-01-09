8c6f27de40c5d8a76b2830fb56aad734257828df
readytotest <117216467+readytotest@users.noreply.github.com>
2024-01-08 23:48:49 -0800

    Clean up comments and make datetime a link on html page

--- js/dateAndTime.js
+++ js/dateAndTime.js
@@ -1,7 +1,9 @@
 /*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+https://developer.mozilla.org/en-US/docs/Web/API/setInterval
 Using slice so the ms is always at three digits
 Using en-ZA format so it uses military time instead of adding a PM between the seconds and ms*/
+
 function timeAndDateRealTime() {
 const getTimeDate = new Date();
 const mill = getTimeDate.getMilliseconds()
@@ -10,8 +12,8 @@ const dateTimePacific = `${getTimeDate.toLocaleString('en-ZA', {
 timeZone: "America/Los_Angeles"})}.${mill3Places}`;
 document.getElementById('#current-time').textContent = dateTimePacific;
 }
-//*https://developer.mozilla.org/en-US/docs/Web/API/setInterval
-//Call the function directly first so there isn't a delay in displaying the text
+
+//Call the function directly first so there isn't a slight delay in displaying the text
 
 timeAndDateRealTime();
 setInterval(timeAndDateRealTime,75);
