a7c2b22aa40a9684a2e95b9a66983a4678323c77
readytotest <117216467+readytotest@users.noreply.github.com>
2024-01-09 14:27:39 -0800

    Add limit parameter on weather site, although i already know it doesnt work. add note about submitting bug report.

--- js/weatherAlert.js
+++ js/weatherAlert.js
@@ -6,7 +6,7 @@ fetch('https://api.weather.gov')
 })
 
 //Weather alert
-fetch('https://api.weather.gov/alerts/active') 
+fetch('https://api.weather.gov/alerts/active?limit=8') 
 .then(response => response.json()) 
 
 .then(response => { document.querySelector("weather-title").textContent = (response.title)
