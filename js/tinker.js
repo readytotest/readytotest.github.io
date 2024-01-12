ae11e58036019dc9382c24295fcc2f4530f1b112
readytotest <117216467+readytotest@users.noreply.github.com>
2024-01-11 18:33:20 -0800

    Add WebStorm to links

---
+++ js/tinker.js
@@ -0,0 +1,23 @@
+//Stuff from practice coding challenges
+export const knightIsAwake = true;
+export function canExecuteFastAttack(knightIsAwake) {
+    if knightIsAwake = true
+    {return true;}
+     else
+     {return false;}
+   
+  }
+  
+
+
+/* ----NOTEPAD-----
+
+if (b === 2)
+      console.log("a is 1 and b is 2");
+  else
+
+
+
+
+
+  */
