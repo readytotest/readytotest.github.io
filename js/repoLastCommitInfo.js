432a46a218ef6a89edc871f2e60f0f3f1641b2a0
readytotest <117216467+readytotest@users.noreply.github.com>
2024-01-13 13:56:31 -0800

    Add source file for repo API

--- js/repoLastCommitInfo.js
+++ js/repoLastCommitInfo.js
@@ -2,6 +2,8 @@ const writeRepoTimeStamp = document.querySelector('repo-update-timestamp');
 
 const myRepoInfo = new XMLHttpRequest();
 myRepoInfo.onreadystatechange = function() {
+  //get ready state and status
+  //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
  if (this.readyState == 4 && this.status == 200) {
     let repoData = JSON.parse(this.responseText);
     writeRepoTimeStamp.innerHTML = (`<b>Repository Last Commit 💾</b><br>`);
@@ -10,7 +12,8 @@ myRepoInfo.onreadystatechange = function() {
     });
  }
  else {
-   // Log the error and display it on the page
+   //Log the error in the console and display it on the page
+   //so there isn't an empty section on the page if the api has a problem
    console.log(`Error fetching repository information. Ready State: ${this.readyState} Status Code: ${this.status}.`);
    writeRepoTimeStamp.innerHTML= `Repository last commit info should be here! There is a problem!<br>Ready State: ${this.readyState}<br>Status Code: ${this.status}<br>`;
  }
