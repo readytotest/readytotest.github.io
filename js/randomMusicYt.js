641eba014ab77077639cce4ab1628c209fff0dfa
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-11 15:07:18 -0700

    Add more songs to playlist

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -13,12 +13,21 @@ const ytPlaylist = [
     "https://youtu.be/sHqs3UGOqO4?feature=shared",
     //Social Distortion - Prison Bound (Guitar Center Sessions)
     "https://youtu.be/lIWHlIhvt3Y?feature=shared",
+    //Mike Ness - Don't Think Twice
+    "https://youtu.be/bcI2hGwTgVs?feature=shared",
     //The Delray Rockets - Rockabilly Man
     "https://youtu.be/pwYx26t-Su4?feature=shared",
     //Alan Jackson - The Older I Get
     "https://youtu.be/kElHR66Y3es?feature=shared",
     //Johnny Cash - Orange Blossom Special (Live at San Quentin)
-    "https://youtu.be/Xhs5j7HN8wM?feature=shared"
+    "https://youtu.be/Xhs5j7HN8wM?feature=shared",
+    //Garth Brooks - Much Too Young to Feel This Damn Old
+    "https://youtu.be/nIHWhUVxJh8?feature=shared",
+    //Kenny Chesney - Save it for a Rainy Day
+    "https://youtu.be/zY9es8R4PD4?feature=shared",
+    //John Lennon - Stand By Me
+    "https://youtu.be/YqB8Dm65X18?feature=shared",
+
 ];
 
 document.getElementById('randomVideoLink').onclick = function() {
