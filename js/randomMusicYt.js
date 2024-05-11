2dc6501e63252b170844a738a20af1492ae26992
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-11 14:23:36 -0700

    Add more songs to Playlist

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -7,6 +7,12 @@ const ytPlaylist = [
     "https://youtu.be/CgkkudRCGB0?feature=shared",
     //Simon - Free at Last (original Mix) (2001)
     "https://youtu.be/HaafC6GSqEU?feature=shared",
+    //Social Distortion - King of Fools (Acoustic)
+    "https://youtu.be/sHqs3UGOqO4?feature=shared",
+    //Social Distortion - Prison Bound (Guitar Center Sessions)
+    "https://youtu.be/lIWHlIhvt3Y?feature=shared",
+    //The Delray Rockets - Rockabilly Man
+    "https://youtu.be/pwYx26t-Su4?feature=shared",
     //Alan Jackson - The Older I Get
     "https://youtu.be/kElHR66Y3es?feature=shared",
     //Johnny Cash - Orange Blossom Special (Live at San Quentin)
