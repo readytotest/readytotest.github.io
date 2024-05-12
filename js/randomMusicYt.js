067b9f1f7b0a6ff784012394ace9662bbbfdd65d
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-11 17:51:16 -0700

    Add more songs to Playlist

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -5,10 +5,14 @@ const ytPlaylist = [
     "https://youtu.be/9MNdOCQYOL8?feature=shared",
     //Pink Floyd - Have a Cigar
     "https://youtu.be/tbdpv7G_PPg?feature=shared",
+    //The Rolling Stones - Honky Tonk Women
+    "https://youtu.be/hqqkGxZ1_8I?feature=shared",
     //Lima Papa - Superlovin' (Original Vox Mix) (2001)
     "https://youtu.be/CgkkudRCGB0?feature=shared",
     //Simon - Free at Last (original Mix) (2001)
     "https://youtu.be/HaafC6GSqEU?feature=shared",
+    //George Satellites - Keep Your Hands to Yourself
+    "https://youtu.be/WonOudGMSdc?feature=shared",
     //Social Distortion - King of Fools (Acoustic)
     "https://youtu.be/sHqs3UGOqO4?feature=shared",
     //Social Distortion - Prison Bound (Guitar Center Sessions)
@@ -19,6 +23,8 @@ const ytPlaylist = [
     "https://youtu.be/pwYx26t-Su4?feature=shared",
     //Alan Jackson - The Older I Get
     "https://youtu.be/kElHR66Y3es?feature=shared",
+    //Zac Brown & Jimmy Buffet - Chicken Fried
+    "https://youtu.be/K2MR5h4eWuE?feature=shared",
     //Brad Paisley - Celebrity
     "https://youtu.be/B9mpDAJOPac?feature=shared",
     //Johnny Cash - Orange Blossom Special (Live at San Quentin)
@@ -29,6 +35,8 @@ const ytPlaylist = [
     "https://youtu.be/zY9es8R4PD4?feature=shared",
     //John Lennon - Stand By Me
     "https://youtu.be/YqB8Dm65X18?feature=shared",
+    //The Beatles - The Ballad of John and Yoko
+    "https://youtu.be/v-1OgNqBkVE?feature=shared",
 
 ];
 
