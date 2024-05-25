1f8f8d121d14d7e52aa7478edec1ec386383c0cb
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-24 17:52:51 -0700

    Update script to check if server is already running as first step

--- start-server.sh
+++ start-server.sh
@@ -13,16 +13,25 @@
 # Press CTRL+C to stop the server
 #-------
 
+# Check if port 3000 is already in use
+if lsof -i:3000 | grep -q LISTEN; then
+  echo "### A service is listening on port 3000 currently." 
+  echo "### Go to http://localhost:3000 and see what happens.."
+  echo "### or type in lsof -i:3000 in your terminal to see what's running."
+  echo "### Script will now exit!"
+  exit 1
+fi
+
 # Start the Node server in the background and store its process ID (PID)
 node server.js &
 NODE_PID=$!
 
 # Print the initial message
-echo "The server is starting... Please wait."
+echo "### The server is attempting to start..."
 
 # Function to handle termination and clean up
 cleanup() {
-  echo "Stopping Node server..."
+  echo "### Stopping Node server..."
   kill $NODE_PID
   exit 1
 }
@@ -30,23 +39,30 @@ cleanup() {
 # Trap SIGINT (Ctrl+C) and call the cleanup function
 trap cleanup SIGINT
 
+echo "### Waiting 5 seconds for server to start."
 sleep 5
 
-# Check if the server is running on port 3000
-if ! lsof -i:3000 | grep -q LISTEN; then
-  echo "Node server failed to start."
-fi
+# Print waiting for server to start message
 
-# Additional comments
-echo "Press CTRL+C to end server OR....."
-echo "lsof -i:3000 <to see the PID of the server>"
-echo "kill -9 (PID goes here remove parenthesis) <to end server>"
+
+# Check if the server is running on port 3000
+if lsof -i:3000 | grep -q LISTEN; then
+  echo "### The server is running on http://localhost:3000"
+  echo "### Press CTRL+C to end server."
+else
+  echo "### Node server failed to start."
+  cleanup
+fi
 
 # Keep the script running until you press Ctrl+C to stop the server
 # If you remove this line, then pressing Ctrl+C won't stop the server
 # because you'll be back at the regular command prompt when the script 
 # finishes running
 wait $NODE_PID
+
+# HELPFUL COMMANDS
+# lsof -i:3000 <to see the PID of the server>"
+# "kill -9 (PID goes here remove parenthesis) <to end server>"
 
 
 
