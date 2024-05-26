a8e5dafb509747c368e49e111fcab4658a7111f6
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-26 12:29:56 -0700

    Update comment

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -7,13 +7,13 @@ to the bottom of the page before the closing body tag
 
 
 const ytPlaylist = [
-    //Pearl Jam - Yellow Ledbetter (Live)
+    //Pearl Jam - Yellow Ledbetter (Live at Madison Square Garden)
     "https://youtu.be/veK0oeGdxLo?feature=shared",
     //Metallica - Whiskey in the Jar (Slane Castle - Meath, Ireland - June 8, 2019)
     "https://youtu.be/9MNdOCQYOL8?feature=shared",
     //Social Distortion - Prison Bound (Guitar Center Sessions)
     "https://youtu.be/lIWHlIhvt3Y?feature=shared",
-    //Alan Jackson - The Older I Get
+    //Alan Jackson - The Older I Get (Official Music Video)
     "https://youtu.be/kElHR66Y3es?feature=shared",
 
 ];
