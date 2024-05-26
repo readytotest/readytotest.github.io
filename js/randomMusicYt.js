dbad179d410efcf412f546402555e5620e3d051e
readytotest <117216467+readytotest@users.noreply.github.com>
2024-05-26 10:35:39 -0700

    Shorten playlist

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -7,30 +7,12 @@ to the bottom of the page before the closing body tag
 
 
 const ytPlaylist = [
-    /* ROCK */
-    //Pearl Jam - Yellow Ledbetter (Acoustic)
-    "https://youtu.be/pJDyJowRlhc?feature=shared",
+    //Pearl Jam - Yellow Ledbetter (Live)
+    "https://youtu.be/veK0oeGdxLo?feature=shared",
     //Metallica - Whiskey in the Jar (Slane Castle - Meath, Ireland - June 8, 2019)
     "https://youtu.be/9MNdOCQYOL8?feature=shared",
-    //Social Distortion - King of Fools (Acoustic)
-    "https://youtu.be/sHqs3UGOqO4?feature=shared",
-    
-    
-    /* HOUSE + ELECTRONIC + DANCE */
-    //Lima Papa - Superlovin' (Original Vox Mix) (2001)
-    "https://youtu.be/CgkkudRCGB0?feature=shared",
-    //Simon - Free at Last (original Mix) (2001)
-    "https://youtu.be/HaafC6GSqEU?feature=shared",
-   
-    
-    /* HIP HOP */
-    //Run DMC ft. Aerosmith - Walk This WAy
-    "https://youtu.be/4B_UYYPb-Gk?feature=shared",
-
-
-    /* COUNTRY */
-    //Johnny Cash - Orange Blossom Special (Live at San Quentin)
-    "https://youtu.be/Xhs5j7HN8wM?feature=shared",
+    //Social Distortion - Prison Bound (Guitar Center Sessions)
+    "https://youtu.be/lIWHlIhvt3Y?feature=shared",
     //Alan Jackson - The Older I Get
     "https://youtu.be/kElHR66Y3es?feature=shared",
 
