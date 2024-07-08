18dbca2325ca6fc93ccf27006ebe0251dc2caf94
readytotest <117216467+readytotest@users.noreply.github.com>
2024-07-07 20:36:00 -0700

    Update comments

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -30,14 +30,41 @@ ___----             ___------              \
                 */     
 
 const ytPlaylist = [
+
+    /*ROCK MUSIC ᕙ(⇀‸↼‶)ᕗ */
     //Pearl Jam - Yellow Ledbetter (Live at Madison Square Garden)
     "https://youtu.be/veK0oeGdxLo?feature=shared",
-    //Simon - Free At Last (Original Mix) (2001)
-    "https://youtu.be/HaafC6GSqEU?feature=shared",
     //Social Distortion - Prison Bound (Guitar Center Sessions)
     "https://youtu.be/lIWHlIhvt3Y?feature=shared",
     //Social Distorion - Ball and Chain (Acoustic)
-    "https://youtu.be/mkfnNUdeo3w?feature=shared"
+    "https://youtu.be/mkfnNUdeo3w?feature=shared",
+    //The Delray Rockets - Rockabilly Man
+    "https://youtu.be/pwYx26t-Su4?feature=shared",
+
+    /*HOUSE MUSIC ᕕ(⌐■_■)ᕗ ♪♬ */
+    //Simon - Free At Last (Original Mix) (2001)
+    "https://youtu.be/HaafC6GSqEU?feature=shared",
+    //Peter Heller - Big Love
+    "https://youtu.be/S1i5D2_XNSw?feature=shared",
+    //Lima Papa - Superlovin' (Original Vox Mix) (2001)
+    "https://youtu.be/CgkkudRCGB0?feature=shared",
+   
+
+    /* COUNTRY MUSIC
+    ,*-.
+    |  |
+,.  |  |
+| |_|  | ,.
+`---.  |_| |
+    |  .--`
+    |  |
+    |  |  */
+    //Alan Jackson - The Older I Get
+    "https://youtu.be/kElHR66Y3es?feature=shared",
+    //Johnny Cash - Orange Blossom Special
+    "https://youtu.be/Xhs5j7HN8wM?feature=shared",
+    //Garth Brookes - Much Too Young to Feel This Damnn Old
+    "https://youtu.be/nIHWhUVxJh8?feature=shared"
 
 ];
 
