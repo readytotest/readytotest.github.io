f4024d775ab503a2330dd858c982a77c9dcc88c6
readytotest <117216467+readytotest@users.noreply.github.com>
2024-07-07 20:14:24 -0700

    Fix comment formatting/alignment

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -16,18 +16,18 @@ ___----             ___------              \
                                   |  \_((_(_)|/(_)
                                   \             (
                                    \_____________)
-                                   ===========================================================
-                                   =                                                         =
-                                   =   This script is used to generate random URLs from an   =
-                                   =   array. Just paste this in your HTML <a href="#"        =
-                                   =   id="randomVideoLink" target="_blank" rel="noopener    =
-                                   =   noreferrer">click here</a> where you want the link to =
-                                   =   appear and then add this <script                      =
-                                   =   src="/yourdirectorypath/randomMusicYt.js"></script> to =
-                                   =   the bottom of the page before the closing body tag.   =
-                                   =                                                         =
-                                   ===========================================================
-                                   */     
+                ===========================================================
+                =                                                         =
+                =   This script is used to generate random URLs from an   =
+                =   array. Just paste this in your HTML <a href="#"        =
+                =   id="randomVideoLink" target="_blank" rel="noopener    =
+                =   noreferrer">click here</a> where you want the link to =
+                =   appear and then add this <script                      =
+                =   src="/yourdirectorypath/randomMusicYt.js"></script> to =
+                =   the bottom of the page before the closing body tag.   =
+                =                                                         =
+                ===========================================================
+                */     
 
 const ytPlaylist = [
     //Pearl Jam - Yellow Ledbetter (Live at Madison Square Garden)
