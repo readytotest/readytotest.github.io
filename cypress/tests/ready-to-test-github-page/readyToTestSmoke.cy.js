ae018ea1e738ed5a1534a93802e21a0f595df153
readytotest <117216467+readytotest@users.noreply.github.com>
2025-02-06 22:19:38 -0800

    test: adjust things to accomodate moving the cypress stuff to its own .htm

--- cypress/tests/ready-to-test-github-page/readyToTestSmoke.cy.js
+++ cypress/tests/ready-to-test-github-page/readyToTestSmoke.cy.js
@@ -10,25 +10,30 @@ describe('Smoke Test GitHub Page', () => {
 
     beforeEach(() => {
 
-        cy.visit('/html/cypress.htm')
-        cy.log('Visit Cypress Page')
+        cy.visit('/')
+        cy.log('Visit Home Page')
       
       })
      
    it('Check for text on page, go to reports page, check for HTML elements', () => {
+      // Check text on the main page
+       cy.get('body').should('include.text', 'National Weather Service API')
+       cy.get('body').should('include.text', 'Postman API Testing')
+       cy.get('body').should('include.text', 'Most Recent Earthquake')
+       cy.get('earthquake-place').should('exist')
+       cy.get('body').should('include.text', 'Repository Last Commit')
+       cy.get('repo-update-timestamp').should('exist')
+
+        // Go to the Cypress page and open the reports page
+        cy.get('[href="/html/cypress.htm"]').click()
         cy.get('[data-cy="cypress-report"]').click() 
         cy.get('center').should('include.text', 'Click on the test results above ^^ to expand it and view the video')
-        cy.get('[href="/html/cypress.htm"]').click() 
-        cy.get('body').should('include.text', 'Ready to Test (QA) GitHub Profile')
+        
+        // Go back to the Cypress page and check some text
+        cy.get('[href="/html/cypress.htm"]').click()
         cy.get('body').should('include.text', 'UI Test Automation with Cypress')
         cy.get('body').should('include.text', 'Cypress Cloud')
-        cy.get('body').should('include.text', 'GitHub Actions')
-        cy.get('body').should('include.text', 'National Weather Service API')
-        cy.get('body').should('include.text', 'Postman API Testing')
-        cy.get('body').should('include.text', 'Most Recent Earthquake')
-        cy.get('earthquake-place').should('exist')
-        cy.get('body').should('include.text', 'Repository Last Commit')
-        cy.get('repo-update-timestamp').should('exist')
+        cy.get('body').should('include.text', 'GitHub Actions')      
 
     
     })
