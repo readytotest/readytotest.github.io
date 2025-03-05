4da02e15eef70946e17e89b7b68fc2be1d3f0126
readytotest <117216467+readytotest@users.noreply.github.com>
2025-03-04 16:32:00 -0800

    chore: add pre-commit hook on staged files with lint-staged, Husky, ESLint, Prettier, and HTML linting

---
+++ eslint.config.cjs
@@ -0,0 +1,38 @@
+//https://eslint.org/docs/latest/rules/
+//https://html-eslint.org/docs/rules
+
+const prettierPlugin = require("eslint-plugin-prettier");
+
+module.exports = [
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      "@html-eslint": require("@html-eslint/eslint-plugin"),
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.cjs"],
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+
+  {
+    files: ["**/*.html", "**/*.htm"],
+    languageOptions: {
+      parser: require("@html-eslint/parser"),
+    },
+    rules: {
+      "@html-eslint/no-duplicate-id": "error",
+    },
+  },
+
+  {
+    ignores: ["node_modules/", ".vscode/*", ".DS_Store", "Thumbs.db"],
+  },
+];
