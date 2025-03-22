df83d203d129946d5fa73e74bfb554d9bd9f20aa
readytotest <117216467+readytotest@users.noreply.github.com>
2025-03-21 20:05:51 -0700

    chore: edit playlist

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -15,8 +15,8 @@
 const ytPlaylist = [
   // Social Distortion - Live Oct 8 2024 - Story of My Life - Bad Luck - Ring of Fire
   "https://youtu.be/Ase-qZaXou0?feature=shared",
-  // AC/DC- Who Made Who (12" Extended Mix)
-  "https://youtu.be/m7PELm_JgVk?feature=shared",
+  // Metallica: Whiskey in the Jar (Castle Donington, England - June 10, 2023)
+  "https://youtu.be/YfouL5PyORU?feature=shared",
   // Pearl Jam “Yellow Ledbetter” Live on the Stern Show
   "https://youtu.be/2cyp1g7idYc?feature=shared",
 ];
