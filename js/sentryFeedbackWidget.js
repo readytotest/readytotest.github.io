020d205cf29f99962d9c9484f130bd90fb6ff6d3
readytotest <117216467+readytotest@users.noreply.github.com>
2025-03-22 12:17:36 -0700

    chore: rename button label and disable no undefined warning

--- js/sentryFeedbackWidget.js
+++ js/sentryFeedbackWidget.js
@@ -1,15 +1,17 @@
+// eslint-disable-next-line no-undef
 Sentry.init({
-    dsn: "https://67cde871b86c2bc34ccf16cbbd00f446@o4506508905938944.ingest.sentry.io/4506508914262016",
-  
-    integrations: [
-      new Sentry.Feedback({
-        // Additional SDK configuration goes in here, for example:
-        colorScheme: "light",
-        buttonLabel: "Send Message",
-        formTitle: "Send Message",
-        submitButtonLabel: "Send Now!",
-        messagePlaceholder: "<Type your message here>",
-        successMessageText: "Message sent successfully!",
-      }),
-    ],
-  });
+  dsn: "https://67cde871b86c2bc34ccf16cbbd00f446@o4506508905938944.ingest.sentry.io/4506508914262016",
+
+  integrations: [
+    // eslint-disable-next-line no-undef
+    new Sentry.Feedback({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "light",
+      buttonLabel: "Let's connect!",
+      formTitle: "Send Message",
+      submitButtonLabel: "Send Now!",
+      messagePlaceholder: "<Type your message here>",
+      successMessageText: "Message sent successfully!",
+    }),
+  ],
+});
