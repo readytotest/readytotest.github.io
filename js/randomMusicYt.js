dd36e67c363fbbc0532a4db7dca3c6e6f8c61351
Eli <117216467+readytotest@users.noreply.github.com>
2025-04-01 19:59:47 -0700

    chore: spinning the playlist wheel for a new track each time

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -15,8 +15,8 @@
 const ytPlaylist = [
   // Social Distortion - Live Oct 8 2024 - Story of My Life - Bad Luck - Ring of Fire
   "https://youtu.be/Ase-qZaXou0?feature=shared",
-  // Metallica: Whiskey in the Jar (Castle Donington, England - June 10, 2023)
-  "https://youtu.be/YfouL5PyORU?feature=shared",
+  // EXCLUSIVE Social Distortion "Prison Bound" Guitar Center Sessions on DIRECTV
+  "https://www.youtube.com/watch?v=lIWHlIhvt3Y",
   // Pearl Jam “Yellow Ledbetter” Live on the Stern Show
   "https://youtu.be/2cyp1g7idYc?feature=shared",
 ];
