5b38707ca1c8c0fbcda83c4a3b70f0e967fcd520
Eli <117216467+readytotest@users.noreply.github.com>
2025-04-05 23:13:48 -0700

    chore: update list

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -15,12 +15,8 @@
 const ytPlaylist = [
   // Social Distortion - Live Oct 8 2024 - Story of My Life - Bad Luck - Ring of Fire
   "https://youtu.be/Ase-qZaXou0?feature=shared",
-  // EXCLUSIVE Social Distortion "Prison Bound" Guitar Center Sessions on DIRECTV
-  "https://www.youtube.com/watch?v=lIWHlIhvt3Y",
   // Pearl Jam “Yellow Ledbetter” Live on the Stern Show
   "https://youtu.be/2cyp1g7idYc?feature=shared",
-  // The Black Crowes - "She Talks to Angels" @ LOCKN' Festival
-  "https://youtu.be/lgYTTM6BfjU?feature=shared",
 ];
 
 document.getElementById("randomVideoLink").onclick = function () {
