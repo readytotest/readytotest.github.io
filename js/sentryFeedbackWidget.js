ecda1827edfffd3ad2e56a70295c9c610787c196
Eli <117216467+readytotest@users.noreply.github.com>
2025-04-10 13:26:57 -0700

    fix: get the widget working on the new version

--- js/sentryFeedbackWidget.js
+++ js/sentryFeedbackWidget.js
@@ -2,13 +2,14 @@ Sentry.init({
   dsn: "https://67cde871b86c2bc34ccf16cbbd00f446@o4506508905938944.ingest.sentry.io/4506508914262016",
 
   integrations: [
-    Sentry.Feedback({
+    Sentry.feedbackIntegration({
       colorScheme: "light",
-      buttonLabel: "Let's connect!",
+      triggerLabel: "Let's connect!",
       formTitle: "Send Message",
       submitButtonLabel: "Send Now!",
       messagePlaceholder: "Type your message here",
       successMessageText: "Message sent successfully!",
+      enableScreenshot: false,
     }),
   ],
 });
