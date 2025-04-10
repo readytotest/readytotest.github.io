a45948c10f25cc5ebdda95b238003809daadfcdf
Eli <117216467+readytotest@users.noreply.github.com>
2025-04-10 15:51:38 -0700

    chore: add link to sentry widget documentation

--- js/sentryFeedbackWidget.js
+++ js/sentryFeedbackWidget.js
@@ -1,3 +1,4 @@
+//https://docs.sentry.io/platforms/javascript/user-feedback/configuration/
 Sentry.init({
   dsn: "https://67cde871b86c2bc34ccf16cbbd00f446@o4506508905938944.ingest.sentry.io/4506508914262016",
 
