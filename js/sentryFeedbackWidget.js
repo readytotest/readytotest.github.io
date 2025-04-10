e20a69140bcea31cc8c7f1ea2f9dba6214acdf99
Eli <117216467+readytotest@users.noreply.github.com>
2025-04-10 13:08:35 -0700

    chore: update Sentry feedback widget integration to v9

--- js/sentryFeedbackWidget.js
+++ js/sentryFeedbackWidget.js
@@ -2,13 +2,12 @@ Sentry.init({
   dsn: "https://67cde871b86c2bc34ccf16cbbd00f446@o4506508905938944.ingest.sentry.io/4506508914262016",
 
   integrations: [
-    new Sentry.Feedback({
-      // Additional SDK configuration goes in here, for example:
+    Sentry.Feedback({
       colorScheme: "light",
       buttonLabel: "Let's connect!",
       formTitle: "Send Message",
       submitButtonLabel: "Send Now!",
-      messagePlaceholder: "<Type your message here>",
+      messagePlaceholder: "Type your message here",
       successMessageText: "Message sent successfully!",
     }),
   ],
