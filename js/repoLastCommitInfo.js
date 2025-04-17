037b38793ea3548a8fb06cf2ebe82d523bf20acd
Eli <117216467+readytotest@users.noreply.github.com>
2025-04-17 16:35:15 -0700

    chore: replace repo.topics with repo.size

--- js/repoLastCommitInfo.js
+++ js/repoLastCommitInfo.js
@@ -28,7 +28,7 @@ fetch("https://api.github.com/users/readytotest/repos")
           const lastCommitMsg = commits[0]?.commit?.message || "No commit message available❕";
           repoTimestamper.insertAdjacentHTML(
             "beforeend",
-            `<a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">${repo.name}</a><br>Topics: ${repo.topics}<br>Timestamp: ${new Date(repo.pushed_at).toLocaleString("en-US", { timeZone: "America/Los_Angeles" })} Pacific<br>git commit -m: "${lastCommitMsg}"<br>`
+            `<a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">${repo.name}</a><br>Repo Size: ${repo.size}KB<br>Timestamp: ${new Date(repo.pushed_at).toLocaleString("en-US", { timeZone: "America/Los_Angeles" })} Pacific<br>git commit -m: "${lastCommitMsg}"<br>`
           );
         })
         .catch((error) => {
