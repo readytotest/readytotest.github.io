843f9311c05ef66fe5bb561373e251807f27d776
eli <117216467+readytotest@users.noreply.github.com>
2025-11-16 16:54:31 -0800

    chore: add more songs

--- js/randomMusicYt.js
+++ js/randomMusicYt.js
@@ -18,11 +18,29 @@ const ytPlaylist = [
   // Billy Idol - Rebel Yell (Rewind Live / 2023)
   "https://youtu.be/wgZJq5mXRqE?feature=shared",
 
-  // Janis Joplin - Piece Of My Heart (1969) Frankfurt, Germany
-  "https://youtu.be/I2x1yo8Ztdw?feature=shared",
+  // Billy Idol – Dancing With Myself – Live From the Empire State Building
+  "https://youtu.be/mWdx7bBWXtE?si=BVyzAcs_31fqRc1z",
 
   // CCR - Born On The Bayou - Live at Woodstock
   "https://youtu.be/_9KKo8mZ5Eo?feature=shared",
+
+  // Eric Church "Some Of It" Live at Freedom Mortgage Pavilion
+  "https://youtu.be/aZjFgimLM6E?si=ceoS6prrANZBONXe",
+
+  // George Jones & Alan Jackson - "He Stopped Loving Her Today" | Opry 100: Country's Greatest Songs
+  "https://youtu.be/Hdz5ue84UMM?si=FgYDfODn3DSjIfvD",
+
+  // Janis Joplin - Piece Of My Heart (1969) Frankfurt, Germany
+  "https://youtu.be/I2x1yo8Ztdw?feature=shared",
+
+ // Jamey Johnson - High Cost of Living (Live at Farm Aid 2021)
+  "https://youtu.be/__xqVoWdzpM?si=KYB_GDNjzNKGtHMk,
+
+  // Johnny Cash - San Quentin (Live at San Quentin, 1969)
+  "https://youtu.be/7e2B-thaJG0?si=XYvhW9C2P8p668MH"
+
+  // John Fogerty - Lodi - acoustic version
+  "https://youtu.be/Wpuh3QkAxwE?si=ZZCqrGoSPreGq0vA",
 
   // Metallica: Enter Sandman (Vienna, Austria - June 1, 2024)
   "https://youtu.be/ZcWkXYzNilY?feature=shared",
@@ -47,6 +65,9 @@ const ytPlaylist = [
 
   // Twisted Sister -  'Twisted Xmas Live In Las Vegas' (2011)
   "https://youtu.be/ujsYUMZQPdw?feature=shared",
+
+  // Willie Nelson - Whiskey River (Live From Austin City Limits, 1981)
+  "https://youtu.be/lPkEmBCZTlA?si=3a4_JuE98DC5Cidl"
 ];
 
 document.getElementById("randomVideoLink").onclick = function () {
